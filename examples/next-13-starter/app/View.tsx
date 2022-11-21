801444a0dda1716b1df89f9c390ae68650c003de
Sam Margalit <sam@margalit.com.au>
2022-11-21 18:47:22 +1100

    Minimal example for compound variants

---
+++ examples/next-13-starter/app/View.tsx
@@ -0,0 +1,4 @@
+"use client"
+import { View as ReshapedView } from 'reshaped'
+export const View = ReshapedView
+export const ViewItem = ReshapedView.Item
--- examples/next-13-starter/app/page.tsx
+++ examples/next-13-starter/app/page.tsx
@@ -1,6 +1,20 @@
-"use client";
-import Demo from "../components/Demo";
+// Issue 1
+// Using View directly in a server component doesn't work
+// import { View } from "reshaped";
+// Re-exporting View in a client component works
+import { View } from "./View";
+
+// Issue 2
+// Using View.Item directly in a server component doesn't work
+// const ViewItem = View.Item
+// Re-exporting View.Item as it's own component in a client component does
+import { ViewItem } from "./View";
 
 export default function Home() {
-  return <Demo />;
+  return (
+    <View direction="row">
+      <ViewItem columns={6}>Hello</ViewItem>  
+      <ViewItem columns={6}>Hello</ViewItem>
+    </View>
+  );
 }
